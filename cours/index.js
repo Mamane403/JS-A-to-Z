1aadcd4f934224a1faa108e408a7a755fe08b185
Mamane403 <mamibra321@yahoo.fr>
2022-06-01 00:54:23 +0100

    Update index.js

--- cours/index.js
+++ cours/index.js
@@ -4,7 +4,7 @@ const  btn2 = document.querySelector("#btn2");
 const response = document.querySelector("p");
 
 questionContenaire.addEventListener("click",()=>{
-  questionContenaire.classList.toggle("questionclicked")
+  questionContenaire.classList.toggle("question-clicked")
 });
 btn1.addEventListener("click", ()=>{
   response.classList.add("show-response")
@@ -13,5 +13,33 @@ btn1.addEventListener("click", ()=>{
 btn2.addEventListener("click", ()=>{
   response.classList.add("show-response")
   response.style.background="green"
+});
+//----------------------------------------------------------
 
+const mousemove = document.querySelector(".mousemove")
+
+window.addEventListener("mousemove", (e)=> {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageX + "px";
+});
+
+window.addEventListener("mousedown",(e)=>{
+  mousemove.style.transform ="scale(2) translate(-25%,-25%)";
+});
+
+window.addEventListener("mouseup",(e)=>{
+  mousemove.style.transform ="scale(1) translate(-50%,-50%)";
+  mousemove.style.border ="2px solid teal";
+});
+
+questionContenaire.addEventListener("mouseenter",(e)=>{
+  questionContenaire.style.background ="rgba(0,0,0,0)";
+});
+
+questionContenaire.addEventListener("mouseout",(e)=>{
+  questionContenaire.style.background ="pink";
+});
+
+response.addEventListener("mouseover",(e)=>{
+  response.style.transform = "rotate(2deg)";
 });
